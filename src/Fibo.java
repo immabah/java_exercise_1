1171616ee1f986e894e8368654eab16341d87634
Immaculee Bahoundje <immaculee.bahoundje@epita.fr>
2022-02-10 23:22:46 +0100

    fibo

---
+++ src/Fibo.java
@@ -0,0 +1,19 @@
+import java.io.InputStreamReader;
+import java.util.Scanner;
+public class Fibo {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Entrez une valeur:");
+            int N = sc.nextInt();
+            sc.nextLine();
+            for(int i = 1 ; i <= N ; i++)
+                System.out.println( "Fibo de "+ i + " = " + fib(i));
+        }
+        private static int fib(int n) {
+            if (n <= 1) return n;
+            else return fib(n-1) + fib(n-2);
+        }
+    }
+
