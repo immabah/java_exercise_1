1eff2868133aa99e9f6ca401d3ac35db38fb8330
Immaculee Bahoundje <immaculee.bahoundje@epita.fr>
2022-02-10 22:36:56 +0100

    boucle_launcher

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,15 +2,15 @@ public class Launcher {
     public static void main(String[] args) {
         System.out.println("Bienvenue");
         java.util.Scanner entre = new java.util.Scanner(System.in);
-        System.out.println("Entrez un element");
-        String var= entre.nextLine();
-        if (var.equals("quit"))
-        {
-            System.exit(1);
-        }
-        else{
-            System.out.println("Unknown command");
+        while (true) {
+            System.out.println("Entrez un element:");
+            String var = entre.nextLine();
+            if (var.equals("quit")) {
+                System.exit(1);
+            } else {
+                System.out.println("Unknown command");
 
+            }
         }
     }
 }
